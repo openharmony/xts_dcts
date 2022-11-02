8be15b9cb7ea9d3432c3391bc0b23a7f680da558
wuxiaodan <wuxiaodan5@huawei.com>
2022-11-02 20:01:26 +0800

    js hap第一次打开，手动授权权限失败
    Signed-off-by: wuxiaodan <wuxiaodan5@huawei.com>

--- distributeddatamgr/jstest/distributed_kv_store/server/hap/src/main/js/MainAbility/pages/index/index.js
+++ distributeddatamgr/jstest/distributed_kv_store/server/hap/src/main/js/MainAbility/pages/index/index.js
@@ -20,14 +20,6 @@ export default {
     },
 
     onInit() {
-        this.grantPermission();
     },
 
-    grantPermission() {
-        console.info('Calc[IndexPage] grantPermission')
-        let context = featureAbility.getContext()
-        context.requestPermissionsFromUser(['ohos.permission.DISTRIBUTED_DATASYNC'], 666, function (result) {
-            console.info('Calc[IndexPage] grantPermission,requestPermissionsFromUser,'+result.requestCode)
-        })
-    },
 }
