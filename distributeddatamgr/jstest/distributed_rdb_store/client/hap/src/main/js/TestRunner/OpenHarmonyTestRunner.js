7ce502674218d41b463c684dedd54e54a772ec63
于小雅 <yuxiaoya@huawei.com>
2023-03-16 03:59:23 +0000

    update TestRunner/OpenHarmonyTestRunner.js.
    Signed-off-by: 于小雅 <yuxiaoya@huawei.com>

--- distributeddatamgr/jstest/distributed_rdb_store/client/hap/src/main/js/TestRunner/OpenHarmonyTestRunner.js
+++ distributeddatamgr/jstest/distributed_rdb_store/client/hap/src/main/js/TestRunner/OpenHarmonyTestRunner.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,32 +24,32 @@ function translateParamsToString(parameters) {
     let targetParams = '';
     for (const key in parameters) {
         if (keySet.has(key)) {
-            targetParams += ' ' + key + ' ' + parameters[key]
+            targetParams += ' ' + key + ' ' + parameters[key];
         }
     }
-    return targetParams.trim()
+    return targetParams.trim();
 }
 
  export default {
     onPrepare() {
-        console.info('OpenHarmonyTestRunner OnPrepare')
+        console.info('OpenHarmonyTestRunner OnPrepare');
     },
     onRun() {
-        console.log('OpenHarmonyTestRunner onRun run')
-        var abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
-        var abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
+        console.log('OpenHarmonyTestRunner onRun run');
+        let abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments();
+        let abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator();
 
-        var testAbilityName = abilityDelegatorArguments.parameters['-p'] + '.TestAbility'
+        let testAbilityName = abilityDelegatorArguments.parameters['-p'] + '.TestAbility';
 
-        var cmd = 'aa start -d 0 -a ' + testAbilityName + ' -b ' + abilityDelegatorArguments.bundleName
-        cmd += ' ' + translateParamsToString(abilityDelegatorArguments.parameters)
-        var debug = abilityDelegatorArguments.parameters["-D"]
-        console.info('debug value : '+debug)
+        let cmd = 'aa start -d 0 -a ' + testAbilityName + ' -b ' + abilityDelegatorArguments.bundleName;
+        cmd += ' ' + translateParamsToString(abilityDelegatorArguments.parameters);
+        let debug = abilityDelegatorArguments.parameters["-D"];
+        console.info('debug value : '+debug);
         if (debug == 'true')
         {
-            cmd += ' -D'
+            cmd += ' -D';
         }
-        console.info('cmd : '+cmd)
+        console.info('cmd : '+cmd);
         abilityDelegator.executeShellCommand(cmd, (err, data) => {
             console.info('executeShellCommand : err : ' + JSON.stringify(err));
             console.info('executeShellCommand : data : ' + data.stdResult);
