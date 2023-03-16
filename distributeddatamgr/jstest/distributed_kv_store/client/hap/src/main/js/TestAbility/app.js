a08b3b3d7830f71724a2a2364410a96e2f301ffb
wuxiaodan <wuxiaodan5@huawei.com>
2023-03-16 07:00:10 +0000

    update distributeddatamgr/jstest/distributed_kv_store/client/hap/src/main/js/TestAbility/app.js.
    Signed-off-by: wuxiaodan <wuxiaodan5@huawei.com>

--- distributeddatamgr/jstest/distributed_kv_store/client/hap/src/main/js/TestAbility/app.js
+++ distributeddatamgr/jstest/distributed_kv_store/client/hap/src/main/js/TestAbility/app.js
@@ -13,17 +13,17 @@
  * limitations under the License.
  */
 
-import AbilityDelegatorRegistry from '@ohos.application.abilityDelegatorRegistry'
-import { Hypium } from '@ohos/hypium'
-import testsuite from '../test/List.test'
+import AbilityDelegatorRegistry from '@ohos.application.abilityDelegatorRegistry';
+import { Hypium } from '@ohos/hypium';
+import testsuite from '../test/List.test';
 
 export default {
     onCreate() {
-        console.info('TestApplication onCreate')
-        var abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
-        var abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
-        console.info('start run testcase!!!')
-        Hypium.hypiumTest(abilityDelegator, abilityDelegatorArguments, testsuite)
+        console.info('TestApplication onCreate');
+        var abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator();
+        var abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments();
+        console.info('start run testcase!!!');
+        Hypium.hypiumTest(abilityDelegator, abilityDelegatorArguments, testsuite);
     },
     onDestroy() {
         console.info("TestApplication onDestroy");
