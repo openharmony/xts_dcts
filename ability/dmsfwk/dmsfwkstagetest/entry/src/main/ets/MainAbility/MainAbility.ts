ab29e8c4d1e6dd2ede1f1c90b581df9d6529e601
zhaominjie11 <zhaominjie9@huawei.com>
2023-11-18 02:17:30 +0000

    update ability/dmsfwk/dmsfwkstagetest/entry/src/main/ets/MainAbility/MainAbility.ts.
    Signed-off-by: zhaominjie11 <zhaominjie9@huawei.com>

--- ability/dmsfwk/dmsfwkstagetest/entry/src/main/ets/MainAbility/MainAbility.ts
+++ ability/dmsfwk/dmsfwkstagetest/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -19,14 +19,17 @@ import rpc from '@ohos.rpc';
 import commonEvent from '@ohos.commonEvent';
 class StubTest extends rpc.RemoteObject {
     constructor(descriptor) {
-        super(descriptor);
+
+        super(descriptor)
     }
+
     onRemoteRequest(code, data, reply, option) {
+
         console.info("onRemoteRequest: " + code)
-        let tmp1 = data.readInt()
-        let tmp2 = data.readInt()
+        let tmp1 = data.readInt();
+        let tmp2 = data.readInt();
         let tmp3 = tmp1 + tmp2;
-        let result =  reply.writeInt(tmp3)
+        let result =  reply.writeInt(tmp3);
         console.info("The server's writeInt result is " + result);
         commonEvent.publish("DmsFwkService_RemoteObject", {
             parameters:{
@@ -42,7 +45,8 @@ export default class MainAbility extends Ability {
     localStorage: LocalStorage;
 
     onCreate(want, launchParam) {
-        console.log("[Demo] MainAbility onCreate")
+
+        console.log("[Demo] MainAbility onCreate");
         globalThis.abilityWant = want;
         if (launchParam.launchReason == AbilityConstant.LaunchReason.CONTINUATION) {
             this.localStorage = new LocalStorage();
@@ -50,8 +54,7 @@ export default class MainAbility extends Ability {
         }
     }
     onConnect(want) {
-        // Called when the form provider is notified that a temporary form is successfully
-        // converted to a normal form.
+
         console.info("DmsFwkStageServer ServiceAbility onConnect")
         let CommonEventPublishData = {
             parameters: {
@@ -65,12 +68,12 @@ export default class MainAbility extends Ability {
     }
 
     onDisconnect(want) {
-        // Called to notify the form provider to update a specified form.
         console.info("DmsFwkStageServer ServiceAbility onDisconnect")
 
     }
 
     onNewWant(want, launchParam) {
+        
         console.log("[Demo] MainAbility onNewWant")
         globalThis.abilityWant = want;
         if (launchParam.launchReason == AbilityConstant.LaunchReason.CONTINUATION) {
@@ -84,31 +87,26 @@ export default class MainAbility extends Ability {
     }
 
     onWindowStageCreate(windowStage) {
-        // Main window is created, set main page for this ability
         console.log("[Demo] MainAbility onWindowStageCreate")
         globalThis.abilityContext = this.context;
         windowStage.setUIContent(this.context, "MainAbility/pages/MainAbility_pages", null)
     }
 
     onWindowStageRestore(windowStage) {
-        // Main window is created, set main page for this ability
         console.log("[Demo] MainAbility onWindowStageRestore")
         globalThis.abilityContext = this.context;
         windowStage.setUIContent(this.context, "MainAbility/pages/MainAbility_pages", null)
     }
 
     onWindowStageDestroy() {
-        // Main window is destroyed, release UI related resources
         console.log("[Demo] MainAbility onWindowStageDestroy")
     }
 
     onForeground() {
-        // Ability has brought to foreground
         console.log("[Demo] MainAbility onForeground")
     }
 
     onBackground() {
-        // Ability has back to background
         console.log("[Demo] MainAbility onBackground")
     }
 
