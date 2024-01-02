c33e91a6df611fdb31d72821730572932c437a8d
liangxiafei <liangxiafei@h-partners.com>
2024-01-02 09:10:56 +0000

    修改警告
    Signed-off-by: liangxiafei <liangxiafei@h-partners.com>

--- distributedhardware/distributedaudiodelaytest/distributedaudiodelaytest.h
+++ distributedhardware/distributedaudiodelaytest/distributedaudiodelaytest.h
@@ -36,21 +36,21 @@ namespace OHOS {
 namespace AudioStandard {
 struct WAV_HEADER {
     /* RIFF Chunk Descriptor */
-    uint8_t RIFF[4] = {'R', 'I', 'F', 'F'}; // RIFF Header Magic header
-    uint32_t ChunkSize = 0; // RIFF Chunk Size
-    uint8_t WAVE[4] = {'W', 'A', 'V', 'E'}; // WAVE Header
+    uint8_t riff[4] = {'R', 'I', 'F', 'F'}; // RIFF Header Magic header
+    uint32_t chunkSize = 0; // RIFF Chunk Size
+    uint8_t wave[4] = {'W', 'A', 'V', 'E'}; // WAVE Header
     /* "fmt" sub-chunk */
     uint8_t fmt[4] = {'f', 'm', 't', ' '}; // FMT header
-    uint32_t Subchunk1Size = 16; // Size of the fmt chunk
-    uint16_t AudioFormat = 1; // Audio format 1=PCM
-    uint16_t NumOfChan = 2; // Number of channels 1=Mono 2=Stereo
-    uint32_t SamplesPerSec = 44100; // Sampling Frequency in Hz
+    uint32_t subchunk1Size = 16; // Size of the fmt chunk
+    uint16_t audioFormat = 1; // Audio format 1=PCM
+    uint16_t numOfChan = 2; // Number of channels 1=Mono 2=Stereo
+    uint32_t samplesPerSec = 44100; // Sampling Frequency in Hz
     uint32_t bytesPerSec = 176400; // bytes per second
     uint16_t blockAlign = 2; // 2=16-bit mono, 4=16-bit stereo
     uint16_t bitsPerSample = 16; // Number of bits per sample
     /* "data" sub-chunk */
-    uint8_t Subchunk2ID[4] = {'d', 'a', 't', 'a'}; // "data"  string
-    uint32_t Subchunk2Size = 0; // Sampled data length
+    uint8_t subchunk2ID[4] = {'d', 'a', 't', 'a'}; // "data"  string
+    uint32_t subchunk2Size = 0; // Sampled data length
 };
 using WavHdr = struct WAV_HEADER;
 void MakeShare();
