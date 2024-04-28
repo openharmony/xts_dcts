a447f1b8daf47e8fcf6b46a7220fa55110121f3e
li-tiangang4 <litiangang4@huawei.com>
2024-04-28 18:05:51 +0800

    dinput修改cppcheck告警
    Signed-off-by: li-tiangang4 <litiangang4@huawei.com>

--- distributedhardware/distributedinputtest/distributed_input_test.cpp
+++ distributedhardware/distributedinputtest/distributed_input_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -82,8 +82,8 @@ void DistributedInputTest::TestStartStopDInputCallback::OnResultDhids(
     return;
 }
 
-void DistributedInputTest::TestInputNodeListener::OnNodeOnLine(const std::string srcDevId,
-    const std::string sinkDevId, const std::string sinkNodeId, const std::string sinkNodeDesc)
+void DistributedInputTest::TestInputNodeListener::OnNodeOnLine(const std::string &srcDevId,
+    const std::string &sinkDevId, const std::string &sinkNodeId, const std::string &sinkNodeDesc)
 {
     (void)srcDevId;
     (void)sinkDevId;
@@ -92,8 +92,8 @@ void DistributedInputTest::TestInputNodeListener::OnNodeOnLine(const std::string
     return;
 }
 
-void DistributedInputTest::TestInputNodeListener::OnNodeOffLine(const std::string srcDevId,
-    const std::string sinkDevId, const std::string sinkNodeId)
+void DistributedInputTest::TestInputNodeListener::OnNodeOffLine(const std::string &srcDevId,
+    const std::string &sinkDevId, const std::string &sinkNodeId)
 {
     (void)srcDevId;
     (void)sinkDevId;
--- distributedhardware/distributedinputtest/distributed_input_test.h
+++ distributedhardware/distributedinputtest/distributed_input_test.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -95,11 +95,11 @@ public:
     public:
         TestInputNodeListener() = default;
         virtual ~TestInputNodeListener() = default;
-        void OnNodeOnLine(const std::string srcDevId, const std::string sinkDevId,
-            const std::string sinkNodeId, const std::string sinkNodeDesc);
+        void OnNodeOnLine(const std::string &srcDevId, const std::string &sinkDevId,
+            const std::string &sinkNodeId, const std::string &sinkNodeDesc);
 
-        void OnNodeOffLine(const std::string srcDevId, const std::string sinkDevId,
-            const std::string sinkNodeId);
+        void OnNodeOffLine(const std::string &srcDevId, const std::string &sinkDevId,
+            const std::string &sinkNodeId);
     };
 
     class TestSimulationEventListenerStub : public
