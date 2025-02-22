3f16d990fe63e22155bebd9fce0a0ee09f939172
yuxiaoya2 <yuxiaoya@huawei.com>
2025-02-22 11:28:21 +0800

    rdb dcts 用例优化 服务端早些执行开库
    Signed-off-by: yuxiaoya2 <yuxiaoya@huawei.com>
    Change-Id: Ib5effa8782b08d40aaac6ac35d3a32ee29cc8be8

--- distributeddatamgr/jstest/distributed_rdb_stage_store/server/entry/src/main/ets/MainAbility/MainAbility.ts
+++ distributeddatamgr/jstest/distributed_rdb_stage_store/server/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -28,6 +28,7 @@ export default class MainAbility3 extends Ability {
         console.log("[Demo] MainAbility onDestroy")
     }
 
+    
     onWindowStageCreate(windowStage) {
         // Main window is created, set main page for this ability
         console.log("[Demo] MainAbility onWindowStageCreate")
--- distributeddatamgr/jstest/distributed_rdb_stage_store/server/entry/src/main/ets/serviceability/ServiceAbility.ts
+++ distributeddatamgr/jstest/distributed_rdb_stage_store/server/entry/src/main/ets/serviceability/ServiceAbility.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -48,6 +48,7 @@ class StubTest extends rpc.RemoteObject {
 }
 export default class ServiceAbility extends UIAbility {
     onCreate(want) {
+        
         // Called to return a FormBindingData object.
         console.info("RdbStageServer ServiceAbility onCreate")
         if(want.parameters != undefined){
